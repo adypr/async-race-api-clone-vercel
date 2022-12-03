437aefc7467e2316de8cac86d79e9bd25ceb749d
Adypr <workady2015@gmail.com>
2022-12-03 22:48:47 +0300

    fix 5

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
+const db = {
+  garage: [
+      {
+          "name": "Tesla",
+          "color": "#e6e6fa",
+          "id": 1,
+      },
+      {
+          "name": "BMW",
+          "color": "#fede00",
+          "id": 2,
+      },
+      {
+          "name": "Mersedes",
+          "color": "#6c779f",
+          "id": 3,
+      },
+      {
+          "name": "Ford",
+          "color": "#ef3c40",
+          "id": 4,
+      },
+  ],
+  winners: [
+      {
+          id: 1,
+          wins: 1,
+          time: 10,
+      }
+  ]
+};
+
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+  return res.status(200).json(db);
 });
+
+
 
 module.exports = router;
