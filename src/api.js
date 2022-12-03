685a3e553229040f8a5aac3509ef9c59e4536a03
Adypr <workady2015@gmail.com>
2022-12-03 22:03:46 +0300

    fix

--- src/api.js
+++ src/api.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const serverless =require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 
 const app = express();
-
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({'dfgd': 'dfsdf'})
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
 });
 
-app.use('/', router);
+app.use(`/.netlify/functions/api`, router);
 
+module.exports = app;
 module.exports.handler = serverless(app);
